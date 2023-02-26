20aae357f60ba03ed09c021bc1e60c7ea1a9aaa6
Alexander Myasnikov <al.next235@gmail.com>
2023-02-26 16:14:30 +0300

    Fixed BaseActivity

--- app/src/main/java/com/dart69/libproject/FirstFragment.kt
+++ app/src/main/java/com/dart69/libproject/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.dart69.libproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.dart69.libproject.databinding.FragmentFirstBinding
 
