9f39e94c8d318320d9e28190dc982c586f547b51
Alexander Myasnikov <al.next235@gmail.com>
2023-03-09 18:22:34 +0300

    Updated BaseViewModels

--- mvvm-core/src/main/java/com/dart69/mvvm_core/coroutines/LifecycleCollector.kt
+++ mvvm-core/src/main/java/com/dart69/mvvm_core/coroutines/LifecycleCollector.kt
@@ -8,27 +8,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.launch
 
-interface LifecycleCollector {
-    fun <T> Flow<T>.collectWithLifecycle(
-        lifecycleOwner: LifecycleOwner,
-        lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
-        collector: FlowCollector<T>
-    )
-
-    class Default : LifecycleCollector {
-        override fun <T> Flow<T>.collectWithLifecycle(
-            lifecycleOwner: LifecycleOwner,
-            lifecycleState: Lifecycle.State,
-            collector: FlowCollector<T>
-        ) {
-            val flow = this
-            lifecycleOwner.lifecycleScope.launch {
-                lifecycleOwner.apply {
-                    repeatOnLifecycle(lifecycleState) {
-                        flow.collect(collector)
-                    }
-                }
-            }
+fun <T> Flow<T>.collectWithLifecycle(
+    lifecycleOwner: LifecycleOwner,
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    collector: FlowCollector<T>,
+) {
+    val flow = this
+    lifecycleOwner.lifecycleScope.launch {
+        lifecycleOwner.repeatOnLifecycle(lifecycleState) {
+            flow.collect(collector)
         }
     }
 }
